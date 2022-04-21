d603656e899fcf0a50a2ee522455ffbd3a939993
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-04-21 11:02:29 -0400

    Done x2

--- Program.cs
+++ Program.cs
@@ -10,15 +10,27 @@ namespace Loops
             int max;
             int input;
             //Reads low number
-            Console.WriteLine("enter a number");
+            Console.WriteLine("Enter a number.");
             while (!Int32.TryParse(Console.ReadLine(), out min))
                 Console.WriteLine("Invalid number, try again.");
             Console.WriteLine($"Thanks for {min}!");
+
+
             //Reads high number
-            Console.WriteLine("enter a larger number:");
-            while (!Int32.TryParse(Console.ReadLine(), out max))
-                Console.WriteLine("Invalid number, try again.");
+            Console.WriteLine("Enter a larger number:");
+            do
+            {
+                while (!Int32.TryParse(Console.ReadLine(), out max))
+                    Console.WriteLine("Invalid number, try again.");
+            
+                if (max <= min)
+                    Console.WriteLine("Error number too low!");
+            
+            
+            } while (max <= min + 1);
             Console.WriteLine($"Thanks for {max}!");
+                
+
             //Reads number in between
             Console.WriteLine($"Enter number in between {min} and {max}");
             do
@@ -27,8 +39,6 @@ namespace Loops
 
                 while (!Int32.TryParse(Console.ReadLine(), out input))
                     Console.WriteLine("Invalid number, try again.");
-
-
 
 
             } while (input <= min-1 || input >= max+1);
